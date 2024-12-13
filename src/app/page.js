4bfe1b8aedec439eabc32cf075e29f8f7dea5f14
amimtiaz <amemtiaj19@gmail.com>
2024-12-14 05:35:07 +0600

    fixing bugs

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 import NavBar from "./NavBar";
 import Logo from "./logo.png";
 import Hero from "./hero";
-import Fotter from "./footer";
 import From from "./from";
 import Protfolio from "./protfolio";
 import ReviewCard from "./reviewcard";
 import Accordion from "./accordion";
 import { Akshar } from "next/font/google";
 import AskSec from "./asksec";
+import Footer from "./footer";
 
 export default function Home() {
 
@@ -24,7 +24,7 @@ export default function Home() {
       <AskSec></AskSec>
 
       <From></From>
-      <Fotter></Fotter>
+      <Footer></Footer>
     </div>
   );
 }
